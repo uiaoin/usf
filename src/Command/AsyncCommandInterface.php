842770d634813658ad04a1300edd0b88470c42f7
uiaoin <uiaoin@outlook.com>
2022-03-29 20:21:23 +0800

    refactor(framework):add monolog

---
+++ src/Command/AsyncCommandInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Command;
+
+/**
+ * 异步 command （message）
+ * Interface AsyncCommandInterface
+ * @package App\Command
+ */
+interface AsyncCommandInterface extends CommandInterface
+{
+
+}
